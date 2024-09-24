f841b6756233a4827c9d54da69b300c9bf6e0a1e
The Wizard of Hahz <info@luvnft.com>
2024-09-24 15:55:44 -0400

    up

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import lottie from "astro-integration-lottie";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://car.arvrtise.com",
+  integrations: [tailwind(), icon(), lottie()],
+  styles: ["./src/styles/index.css"],
+});
