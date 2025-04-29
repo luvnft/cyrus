a72d877f6b3dea2bbb47868db7e6ac3d6e25d921
The Wizard of Hahz <info@luvnft.com>
2025-04-29 18:06:32 -0400

    up

--- __unconfig_astro.config.mjs
+++ __unconfig_astro.config.mjs
@@ -1,11 +1,17 @@
+
+let __unconfig_data;
+let __unconfig_stub = function (data = {}) { __unconfig_data = data };
+__unconfig_stub.default = (data = {}) => { __unconfig_data = data };
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import lottie from "astro-integration-lottie";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
+const __unconfig_default =  defineConfig({
   site: "https://jersey.fm",
   integrations: [tailwind(), icon(), lottie()],
   styles: ["./src/styles/index.css"],
 });
+
+if (typeof __unconfig_default === "function") __unconfig_default(...[]);export default __unconfig_data;
